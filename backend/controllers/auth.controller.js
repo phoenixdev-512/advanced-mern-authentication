234e2cbeff8381c4bee47c5f0d0fcd6b9644e469
Phoenix512 <abhaybs1729@gmail.com>
2025-03-21 23:22:37 +0530

    add email verification endpoint and welcome email functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,6 +54,31 @@ export const signup = async (req, res) => {
     }
 };
 
+export const verifyEmail = async (req, res) => {
+    //1 2 3 4 5 6 7 8
+    const {code} = req.body;
+    try{
+        const user = await User.findOne( {
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() }
+        })
+
+        if(!user) {
+            return res.status(400).json({success: false, message: "Invalid or expired verification code"})
+        }
+
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+        await user.save();
+
+        await sendWelcomeEmail(user.email, user.name);
+
+    } catch (error) {
+
+    }
+}
+
 export const login = async (req, res) => {
     res.send("login route");
 };
--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -21,3 +21,5 @@ export const sendVerificationEmail = async (email, verificationToken) => {
         throw new Error('Error sending verification email: ${error}');
     };
 }
+
+export const sendWelcomeEmail =  async 
