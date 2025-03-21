655b2b1097aa0a01ba1d99cf2beac2edac3abd15
Phoenix512 <abhaybs1729@gmail.com>
2025-03-21 23:06:35 +0530

    add email verification functionality with OTP

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { User } from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
+import { sendVerificationEmail } from '../mailtrap/emails.js';
 
 export const signup = async (req, res) => {
     const { email, password, name } = req.body;
@@ -35,6 +36,8 @@ export const signup = async (req, res) => {
 
         // Generate token & set cookie
         generateTokenAndSetCookie(res, user._id);
+
+        await sendVerificationEmail(user.email, verificationToken)
 
         res.status(201).json({
             success: true,
--- backend/mailtrap/mailtrap.config.js
+++ backend/mailtrap/mailtrap.config.js
@@ -6,26 +6,13 @@ dotenv.config();
 const TOKEN = process.env.MAILTRAP_TOKEN;
 const ENDPOINT= process.env.MAILTRAP_ENDPOINT;
 
-const client = new MailtrapClient({
+export const mailtrapClient = new MailtrapClient({
   token: TOKEN,
 });
 
-const sender = {
+export const sender = {
   email: "hello@demomailtrap.co",
   name: "phoenix",
 };
-const recipients = [
-  {
-    email: "abhaybs1729@gmail.com",
-  }
-];
 
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
+
