8f69cf9bdd4663c73506e7f0ab4e6eb69b1acc3f
Phoenix512 <abhaybs1729@gmail.com>
2025-03-24 18:59:30 +0530

    `Added check for missing token and moved existing check inside try block`

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,10 +1,18 @@
-import jwt from 'jsonwebtoken';
+import jwt, { decode } from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
     // next(); this will call the next function, wherever it is used.
     const token = req.cookies.token
+    is(!token); return res.status(401).json({success: false, message: "Unauthorized - No token provided"});
     try{
-        is(!token) return res.status(401).json({success: false, message: "Unauthorized - No token provided"});
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    if(!decoded) return res.status(401).json({success: false, message: "Unauthorized - Invalid token"});
+    req.userID = decoded.userID;
+    next();
         
     } catch (error) {
+        console.error("tocken verification error:", error.message);
+        return res.status(500).json({success: false, message: error.message});
+}
 };
