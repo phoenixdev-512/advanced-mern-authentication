c68e8b9ac14000965412d9c80da84c88d545d531
Phoenix512 <abhaybs1729@gmail.com>
2025-03-24 20:24:55 +0530

    FIXED UNREACHABLE CODE SHIT IDK HOW

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -3,8 +3,8 @@ import jwt, { decode } from 'jsonwebtoken';
 export const verifyToken = (req, res, next) => {
     // next(); this will call the next function, wherever it is used.
     const token = req.cookies.token
-    is(!token); return res.status(401).json({success: false, message: "Unauthorized - No token provided"});
-    try{
+    if(!token) return res.status(401).json({success: false, message: "Unauthorized - No token provided"});
+    try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     
     if(!decoded) return res.status(401).json({success: false, message: "Unauthorized - Invalid token"});
