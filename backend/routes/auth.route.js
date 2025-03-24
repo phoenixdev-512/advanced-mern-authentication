b00a29d98f1087264cb2dc1639f7004818720404
Phoenix512 <abhaybs1729@gmail.com>
2025-03-24 17:08:31 +0530

    feat: add resetPassword route to authentication endpoints

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, signup, verifyEmail, forgotPassword } from '../controllers/auth.controller.js';
+import { login, logout, signup, verifyEmail, forgotPassword, resetPassword } from '../controllers/auth.controller.js';
 
 // const router = express.Router(); is used to create a new router object in the auth.route.js file.
 // The router object is used to define the routes for the authentication endpoints, such as /signup, /login, /logout, /verify-email, and /forgot-password.  
@@ -13,6 +13,8 @@ router.post("/logout", logout)
 
 router.post("/verify-email", verifyEmail);
 router.post("/forgot-password", forgotPassword);
+
+router.post("/reset-password/:token", resetPassword);
 
 export default router;
 // auth.route.js main function is to define the routes for the authentication endpoints.
